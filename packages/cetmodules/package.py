bae580bfe5eb18e078b5602a319cb5f62ab8b5b0
Chris Green <greenc@fnal.gov>
2022-09-22 09:28:42 -0500

    cetmodules: New variants, overhauled dependencies and build options
    * Add new Perl dependencies to enable UPS-style operation and tests
      thereof.
    
    * Documentation generation:
    
      * New variants `docs`, `versioned-docs`.
    
      * New Python dependencies on Sphinx and related packages.
    
      * Accommodate variants in configure options.
    
    * Handle presets in configure options

--- packages/cetmodules/package.py
+++ packages/cetmodules/package.py
@@ -23,18 +23,53 @@ class Cetmodules(CMakePackage):
     version("develop", branch="develop", get_full_repo=True)
     version("3.19.02", sha256="214172a59f4c3875a5d7c2617b9f50ed471c86404d85e2e5c72cadf5b499cdc6")
 
-    depends_on("cmake@3.11:", type="build", when="@:1.01.99")
-    depends_on("cmake@3.12:", type="build", when="@1.02.00:")
-    depends_on("cmake@3.18:", type="build", when="@2.07.00:")
-    depends_on("py-sphinxcontrib-moderncmakedomain", when="@2.00.10:", type="build")
-    depends_on("py-sphinxcontrib-moderncmakedomain", when="@develop", type="build")
-    depends_on("py-sphinx-rtd-theme", when="@2.00.10:", type="build")
-    depends_on("py-sphinx-rtd-theme", when="@develop", type="build")
+    variant("versioned-docs", default=False, description="build versioned docs with a landing page")
+    variant("docs", default=False, when="~versioned-docs", description="build documentation")
+
+    depends_on("cmake@3.21:", type="build")
+    docs_deps = (
+        "git@2.22:",
+        "py-sphinxcontrib-moderncmakedomain",
+        "py-sphinx-rtd-theme",
+        "py-sphinx-design@0.2.0:",
+        "py-sphinx@5:5.999"
+    )
+    for dep in docs_deps:
+        depends_on(dep, type="build", when="+docs")
+        depends_on(dep, type="build", when="+versioned-docs")
+
+    perl_deps = {
+        "perl-data-dumper" : ("build", "run"),
+        "perl-json" : ("build", "run"),
+        "perl-list-moreutils" : ("build", "run"),
+        "perl-metacpan-client" : ("build", "run"),
+        "perl-path-tiny" : ("build", "run"),
+        "perl-readonly" : ("build", "test", "run"),
+        "perl-perl-tidy": ("build", "test", "run"),
+        "perl-task-perl-critic": ("build", "test")
+    }
+
+    for pkg, types in perl_deps.items():
+        depends_on(pkg, type=types)
+
+    conflicts("@:3.19.01", when="^cmake@3.24.0:")
 
     @run_before("cmake")
     def fix_fix_man(self):
         filter_file(r"exit \$status", "exit 0", "%s/libexec/fix-man-dirs" % self.stage.source_path)
 
+    def cmake_args(self):
+        options = [ "--preset", "default" ]
+        if not any([ self.spec.variants[doc_opt].value
+                     for doc_opt in ("docs", "versioned-docs")
+                     if doc_opt in self.spec.variants ]):
+            options.append(self.define("BUILD_DOCS", False))
+        elif self.spec.variants["versioned-docs"].value:
+            options += [ self.define("{0}_SPHINX_DOC_PUBLISH_VERSION_BRANCH".format(self.name),
+                                     self.spec.version),
+                         self.define("{0}_SPHINX_DOC_PUBLISH_ROOT".format(self.name),
+                                     join_path(self.stage.path, "doc_root")) ]
+        return options
 
     def setup_run_environment(self, run_env):
         run_env.prepend_path("CMAKE_PREFIX_PATH", "{0}/Modules".format(self.prefix))
