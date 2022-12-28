2d1bfbaa9de21385f0a2fe43add9a9502db6838a
Eric Flumerfelt <eflumerf@fnal.gov>
2022-12-28 14:37:03 -0600

    Add otsdaq packages and XDAQ dependnecy

---
+++ packages/artdaq-database/package.py
@@ -0,0 +1,47 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+import sys
+
+from spack import *
+
+
+class ArtdaqDatabase(CMakePackage):
+    """The toolkit currently provides functionality for data transfer,
+    event building, event reconstruction and analysis (using the art analysis
+    framework), process management, system and process state behavior, control
+    messaging, local message logging (status and error messages), DAQ process
+    and art module configuration, and the writing of event data to disk in ROOT
+    format."""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/artdaq/wiki"
+    url = "https://github.com/art-daq/artdaq_database/archive/refs/tags/v3_09_03.tar.gz"
+    git = "https://github.com/art-daq/artdaq_database.git"
+
+    version("develop", branch="eflumerf/Spack", get_full_repo=True)
+
+    variant(
+        "cxxstd",
+        default="17",
+        values=("14", "17"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+    variant("builtin_fhicl", default=True, description="Use built-in FHiCL-cpp with database fixes")
+
+    depends_on("swig", type="build")
+    depends_on("cetmodules", type="build")
+    depends_on("trace")
+    depends_on("curl")
+    depends_on("boost+filesystem+program_options")
+    depends_on("cetlib", when="~builtin_fhicl")
+    depends_on("cetlib-except", when="~builtin_fhicl")
+    depends_on("fhicl-cpp", when="~builtin_fhicl")
+
+    def cmake_args(self):
+        args = [self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
+                "-DUSE_FHICLCPP={0}".format("TRUE" if "+builtin_fhicl" in self.spec else "FALSE")]
+        return args
---
+++ packages/otsdaq-epics/package.py
@@ -0,0 +1,38 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+import sys
+
+from spack import *
+
+
+class OtsdaqEpics(CMakePackage):
+    """The toolkit currently provides functionality for data transfer,
+    event building, event reconstruction and analysis (using the art analysis
+    framework), process management, system and process state behavior, control
+    messaging, local message logging (status and error messages), DAQ process
+    and art module configuration, and the writing of event data to disk in ROOT
+    format."""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/artdaq/wiki"
+    url = "https://github.com/art-daq/otsdaq_epics/archive/refs/tags/v3_09_03.tar.gz"
+    git = "https://github.com/art-daq/otsdaq_epics.git"
+
+    version("develop", branch="develop", get_full_repo=True)
+
+    variant(
+        "cxxstd",
+        default="17",
+        values=("14", "17"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+
+    depends_on("otsdaq")
+    depends_on("otsdaq-utilities")
+    depends_on("epics")
+    depends_on("pqxx")
+    depends_on("cetmodules", type="build")
