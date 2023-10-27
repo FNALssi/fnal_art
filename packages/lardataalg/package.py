47758afb17ab28b7873bdb9304cb39672ef03fdc
Marc Mengel <mengel@fnal.gov>
2023-10-27 16:10:10 -0500

    last few

--- packages/lardataalg/package.py
+++ packages/lardataalg/package.py
@@ -82,6 +82,14 @@ class Lardataalg(CMakePackage):
     patch("v09_07_00.patch", when="@09.07.00")
     patch("v09_07_02.patch", when="@09.07.02")
 
+    def patch(self):
+        filter_file(
+            '#include <string>',
+            '#include <cstdint>\n#include <string>',
+            'lardataalg/DetectorInfo/RunHistoryStandard.h',
+        )
+
+
     variant(
         "cxxstd",
         default="17",
