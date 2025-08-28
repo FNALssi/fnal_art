6fafec9c6972a0b3ba38829479b8f1cbf0bf2d76
Patrick Gartung <gartung@fnal.gov>
2025-08-28 11:47:54 -0500

    Make genie depend on tpythia6 instead of using pythia variant

--- spack_repo/fnal_art/packages/genie/package.py
+++ spack_repo/fnal_art/packages/genie/package.py
@@ -76,8 +76,6 @@ class Genie(AutotoolsPackage):
         description="Use the specified C++ standard when building.",
     )
 
-    variant("pythia6", default=True)
-
     variant("lhapdf", default=True) 
 
     depends_on("c", type="build")
@@ -87,13 +85,13 @@ class Genie(AutotoolsPackage):
     depends_on("lhapdf" , when="+lhapdf")
 
     # Issues caused by default root cxxstd being 11
-    depends_on("root @6.24.08:6.28.12 +pythia6 cxxstd=14", when="cxxstd=14")
-    depends_on("root @6.24.08:6.28.12 +pythia6 cxxstd=17", when="cxxstd=17")
-    depends_on("root @6.24.08:6.28.12 +pythia6 cxxstd=17", when="cxxstd=default")
-    depends_on("root @6.24.08:6.28.12 +pythia6 cxxstd=20", when="cxxstd=20 +pythia6")
-    depends_on("root cxxstd=20", when="cxxstd=20 ~pythia6")
+    depends_on("root cxxstd=14", when="cxxstd=14")
+    depends_on("root cxxstd=17", when="cxxstd=17")
+    depends_on("root cxxstd=17", when="cxxstd=default")
+    depends_on("root cxxstd=20", when="cxxstd=20")
 
-    depends_on("pythia6+root", when="+pythia6")
+    depends_on("tpythia6")
+    depends_on("pythia6+root")
     depends_on("libxml2")
     depends_on("log4cpp")
     depends_on("gsl")
