c02215948cd4fb0be64a59fdebd5645397579cc5
Patrick Gartung <gartung@fnal.gov>
2025-08-29 14:29:22 -0500

    Still depends_on("c") for tests

--- spack_repo/fnal_art/packages/fhicl_cpp/package.py
+++ spack_repo/fnal_art/packages/fhicl_cpp/package.py
@@ -35,7 +35,7 @@ class FhiclCpp(CMakePackage, FnalGithubPackage):
     conflicts("cxxstd=17", when="@4.19.00:")
 
     depends_on("cxx", type="build")
-    depends_on("c", when="@:4.19.01", type="build")
+    depends_on("c", type="build")
     depends_on("boost@:1.82", when="@:4.19")
     depends_on("boost+program_options+test")
     depends_on("cetlib")
