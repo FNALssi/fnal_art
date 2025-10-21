a2905df5f23f928f7c6f4e50ee8684d9a89ddbec
Patrick Gartung <gartung@fnal.gov>
2025-10-21 12:30:16 -0500

    llvm is only needed to build castxml

--- spack_repo/fnal_art/packages/castxml/package.py
+++ spack_repo/fnal_art/packages/castxml/package.py
@@ -54,7 +54,7 @@ class Castxml(CMakePackage):
     depends_on("cxx", type="build")
 
     # FIXME: Add dependencies if required.
-    depends_on('llvm')
+    depends_on('llvm', type="build")
 
     def cmake_args(self):
         # FIXME: Add arguments other than
