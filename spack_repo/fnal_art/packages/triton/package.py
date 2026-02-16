60af82ece3a9cf16e24facee94145c0d7b5750ab
Patrick Gartung <gartung@fnal.gov>
2026-02-16 15:33:45 -0600

    Remove constraints on triton so solution can be reached.

--- spack_repo/fnal_art/packages/triton/package.py
+++ spack_repo/fnal_art/packages/triton/package.py
@@ -43,8 +43,8 @@ class Triton(CMakePackage):
 
     depends_on("py-setuptools", type="build")
     depends_on("py-wheel", type="build")
-    depends_on("py-grpcio@1.63:1.67", type=("build", "run"))
-    depends_on("py-grpcio-tools@1.63:1.67", type=("build", "run"))
+    depends_on("py-grpcio", type=("build", "run"))
+    depends_on("py-grpcio-tools", type=("build", "run"))
     depends_on("py-numpy", type=("build","run"))
     depends_on("py-geventhttpclient", type=("build","run"))
     depends_on("py-python-rapidjson", type=("build","run"))
